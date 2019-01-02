cd3c3a100dc9095d46612982cea03056a3cb214e
Anatolii <dinexpod@gmail.com>
2019-01-02 22:54:31 +0200

    -create pull request

---
+++ src/main/java/Connection/ConnectionUtil.java
@@ -0,0 +1,27 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static Connection connection = null;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/coolcompany?serverTimezone=UTC", "root", "root");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problems with JDBC DRIVERS");
+            e.printStackTrace();
+            System.exit(1);
+        } catch (SQLException e) {
+            System.out.println("Problems with connection to  DATABASE");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
